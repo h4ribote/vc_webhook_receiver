80085a6b41c290fc6f346615d3b3a0449988a8e2
haribote <116637919+h4ribote@users.noreply.github.com>
2025-02-21 19:43:00 +0900

    Update api.py

--- vc_webhook_receiver/api.py
+++ vc_webhook_receiver/api.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, Request
 import config
 from discord_interactions import verify_key as webhook_verify
 from webhook_class import WebhookPost
+from time import time
 
 router = APIRouter()
 
@@ -14,6 +15,9 @@ async def Webhook_post(request: Request, webhook: WebhookPost):
 
     if not webhook_verify(request_body, request_signature, request_timestamp, config.VirtualCrypto.PUBLIC_KEY):
         raise HTTPException(401, "Bad request signature")
+
+    if abs(time() - request_timestamp) > 20:
+        raise HTTPException(401, "Your watch is broken")
 
     if webhook.type == 1: # PING
         return {"type":1} # PONG
